72299c2614df6a7f1e5a56058305c8f12c60bff7
LinThit27 <linthit2745@gmail.com>
2024-03-14 16:30:26 +0630

    [install] entity framework

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -3,7 +3,8 @@
     public class Owner
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; }
         public ICollection<PokemonOwner> PokemonOwner { get; set; }    
